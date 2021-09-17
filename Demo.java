af4b26c06622b2c0e84fa68a9601ce1ba4e2acfc
Tabassum Jahan <tabassumsaifi09@gmail.com>
2021-09-17 10:55:48 +0530

    Create Demo.java

---
+++ Demo.java
@@ -0,0 +1,10 @@
+project1 
+Email Application
+Scenrion: you are IT Support Administrator Specialist and are charged with the task of creating email accounts for new hires.
+  
+  your application should do the following:
+Generate an email with the following syntax: firstname.lastname@department.company.com
+Detrmine the department(Sales. development, accounting) if none leave blank
+Generate a random string for a password
+Have set methods to change the password. set the mailbox capacity, and define an alternate email address
+Have get mwthods to display the name, email, and mailbox capacity
